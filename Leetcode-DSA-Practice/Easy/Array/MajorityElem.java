0a9c3db4d0d7cea177767db2f4aa8b7b28cfe246
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-05-12 22:55:12 +0530

    Update MajorityElem.java

--- Leetcode-DSA-Practice/Easy/Array/MajorityElem.java
+++ Leetcode-DSA-Practice/Easy/Array/MajorityElem.java
@@ -3,7 +3,7 @@ package Easy.Array;
 public class MajorityElem {
     static int majorityFn(int[] nums) { //Moor's Voting algorithm  TC - O(n)
         int freq=0, ans=0;
-        for(int i =0; i<nums.length; i++) {
+        for(int i =0; i<nums.length; i++) { 
             if(freq == 0) {
                 ans = nums[i];
             }
